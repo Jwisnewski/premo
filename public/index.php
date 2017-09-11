691c8eea9b3ba3f4e81893c901b988f63c25d6cc
Jeff Wisnewski <jeffrey.joseph@dm-companies.com>
2017-09-11 13:50:39 -0400

    Fix Formatting

--- public/index.php
+++ public/index.php
@@ -1,1 +1,133 @@
-<?php phpinfo() ?>
+<?php
+
+use Phalcon\Loader;
+
+// ...
+
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        "../app/controllers/",
+        "../app/models/",
+    ]
+);
+
+$loader->register();
+
+use Phalcon\Di\FactoryDefault;
+
+// ...
+
+// Create a DI
+$di = new FactoryDefault();
+
+use Phalcon\Mvc\View;
+
+// ...
+
+// Setup the view component
+$di->set(
+    "view",
+    function () {
+        $view = new View();
+
+        $view->setViewsDir("../app/views/");
+
+        return $view;
+    }
+);
+
+use Phalcon\Mvc\Url as UrlProvider;
+
+// ...
+
+// Setup a base URI so that all generated URIs include the "tutorial" folder
+$di->set(
+    "url",
+    function () {
+        $url = new UrlProvider();
+
+        $url->setBaseUri("/tutorial/");
+
+        return $url;
+    }
+);
+
+use Phalcon\Mvc\Application;
+
+// ...
+
+$application = new Application($di);
+
+$response = $application->handle();
+
+$response->send();
+
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+
+
+
+// Register an autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        "../app/controllers/",
+        "../app/models/",
+    ]
+);
+
+$loader->register();
+
+
+
+// Create a DI
+$di = new FactoryDefault();
+
+// Setup the view component
+$di->set(
+    "view",
+    function () {
+        $view = new View();
+
+        $view->setViewsDir("../app/views/");
+
+        return $view;
+    }
+);
+
+// Setup a base URI so that all generated URIs include the "tutorial" folder
+$di->set(
+    "url",
+    function () {
+        $url = new UrlProvider();
+
+        $url->setBaseUri("/tutorial/");
+
+        return $url;
+    }
+);
+
+
+
+$application = new Application($di);
+
+try {
+    // Handle the request
+    $response = $application->handle();
+
+    $response->send();
+} catch (\Exception $e) {
+    echo "Exception: ", $e->getMessage();
+}
+
+
+
+?>
