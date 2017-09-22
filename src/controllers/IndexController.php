79c17cfb09db2af19bc75f7a69f1b15c34614be8
Jeff Wisnewski <jeffrey.joseph@dm-companies.com>
2017-09-22 15:06:28 -0400

    get up to date

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -1,11 +1,34 @@
 <?php
- namespace Movies;
+namespace Premo\Controllers;
 
 use Phalcon\Mvc\Controller;
+use Premo\Services\FetchMovies;
 
 class IndexController extends Controller
 {
     public function indexAction()
     {
+        $fetcher = new FetchMovies();
+        $movies_array = $fetcher->getLatestMovie();
+
+        passToDb($movies_array);
+    }
+
+
+    /**
+     * @param $movies_array
+     */
+    public function passToDb($movies_array){
+
+        $i=0;
+        for($i=0; $i->count($movies_array); $i++)
+        {
+            $movie = Movie::findFirst($movies_array[$i]->$id);
+
+            if(empty($movie)){
+                $movies_array[$i]->save();
+            }
+
+        }
     }
 }
