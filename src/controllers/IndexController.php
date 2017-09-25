552ab6ee7217207b681add5da9859650ac16ff72
Jeff Wisnewski <jeffrey.joseph@dm-companies.com>
2017-09-25 14:41:31 -0400

    index view and phalcon controller class viewing

--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -12,24 +12,23 @@ class IndexController extends Controller
     {
         $fetcher = new FetchMovies();
         $movies_array = $fetcher->getUpcomingMovies();
-
         $this->passToDb($movies_array);
+        $this->view->movies = $movies_array;
     }
 
 
     /**
      * @param $movies_array
      */
-    public function passToDb($movies_array)
+    protected function passToDb($movies_array)
     {
-
         for ($i = 0; $i < count($movies_array); $i++) {
             $movie = Movie::findFirst($movies_array[$i]->id);
-
             if (empty($movie)) {
                 $movies_array[$i]->save();
             }
 
         }
     }
+
 }
