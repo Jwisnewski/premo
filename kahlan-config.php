01f24d30e14acc0bc32ca8d6464e0c9dee7c4812
Jeff Wisnewski <jeffrey.joseph@dm-companies.com>
2017-09-26 11:02:13 -0400

    Integrated Phalcon and Kahlan

--- kahlan-config.php
+++ kahlan-config.php
@@ -1,5 +1,20 @@
 <?php
 use Kahlan\Filter\Filter;
+use Kahlan\Jit\Interceptor;
+use Kahlan\Jit\Patcher\Layer;
+
+Filter::register('api.patchers', function($chain) {
+    $interceptor = Interceptor::instance();
+    $patchers = $interceptor->patchers();
+    $patchers->add('layer', new Layer([
+        'override' => [
+            \Phalcon\Mvc\Model::class
+        ]
+    ]));
+
+    return $chain->next();
+});
+
 
 Filter::register('mycustom.namespaces', function ($chain) {
 
@@ -10,3 +25,4 @@ Filter::register('mycustom.namespaces', function ($chain) {
 });
 
 Filter::apply($this, 'namespaces', 'mycustom.namespaces');
+Filter::apply($this, 'patchers', 'api.patchers');
